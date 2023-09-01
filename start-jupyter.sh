b1ab88a19ffa294c29e93a746a2355682a1ab8c7
Nikola Galiot <galiot96@gmail.com>
2023-09-01 23:14:21 +0200

    make it easy to run jupyter on systems with and without gpu, gitignore created

--- start-jupyter.sh
+++ start-jupyter.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-sudo docker run -it --gpus=all --rm -v ${PWD}/notebooks:/tf/notebooks -v ${PWD}/models:/tf/models -v ${PWD}/data:/tf/data -p 8888:8888 tensorflow/tensorflow:latest-gpu-jupyter
+
+# Initialize a variable to hold the GPU-specific options
+GPU_OPTIONS=""
+
+# Check if "--gpu" argument is passed
+if [[ "$1" == "--gpu" ]]; then
+    GPU_OPTIONS="--gpus=all"
+    IMAGE="tensorflow/tensorflow:latest-gpu-jupyter"
+else
+    IMAGE="tensorflow/tensorflow:latest-jupyter"
+fi
+
+# Run the docker command
+docker run -it $GPU_OPTIONS --rm \
+    -v ${PWD}/notebooks:/tf/notebooks \
+    -v ${PWD}/models:/tf/models \
+    -v ${PWD}/data:/tf/data \
+    -p 8888:8888 \
+    $IMAGE
